af35f33d427811375dd098f41ec09b6fb5834d44
Aleksey Shmidt <lp8839@yandex.ru>
2022-12-30 19:01:16 +0300

    lesson16

---
+++ src/Shmidt/lesson16_enum/Day.java
@@ -0,0 +1,7 @@
+package Shmidt.lesson16_enum;
+
+public class Day {
+    public DaysOfWeekEnum dayOfWeek;
+    public int dayNumber;
+}
+//Создать класс Day c полем String dayOfWeek и поле с номером относительно начала месяца (не статические).
---
+++ src/Shmidt/lesson16_enum/DaysOfWeekEnum.java
@@ -0,0 +1,11 @@
+package Shmidt.lesson16_enum;
+
+public enum DaysOfWeekEnum {
+    понедельник,
+    вторник,
+    среда,
+    четверг,
+    пятница,
+    суббота,
+    воскресенье
+}
