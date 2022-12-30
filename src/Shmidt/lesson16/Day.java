3c8afc37f9f95b3c4f79073995f2bf88fffef5fd
Aleksey Shmidt <lp8839@yandex.ru>
2022-12-30 19:01:16 +0300

    lesson16

---
+++ src/Shmidt/lesson16/Day.java
@@ -0,0 +1,7 @@
+package Shmidt.lesson16;
+
+public class Day {
+    public String dayOfWeek;
+    public int dayNumber;
+}
+//Создать класс Day c полем String dayOfWeek и поле с номером относительно начала месяца (не статические).
---
+++ src/Shmidt/lesson16/DaysOfWeek.java
@@ -0,0 +1,12 @@
+package Shmidt.lesson16;
+
+public class DaysOfWeek {
+    static final String monday ="понедельник";
+    static final String tuesday ="вторник";
+    static final String wednesday ="среда";
+    static final String thursday ="четверг";
+    static final String friday ="пятница";
+    static final String saturday ="суббота";
+    static final String sunday ="воскресенье";
+}
+//    Создать класс DaysOfWeek cо статическими финальными полями - днями недель (Значения: "Понедельник", "Вторник" и тд)
