92adb2a059ca034b277982b80652a90b5f67cce4
Aleksey Shmidt <lp8839@yandex.ru>
2023-01-10 02:22:16 +0300

    lesson19

---
+++ src/Shmidt/lesson_19/Cat.java
@@ -0,0 +1,65 @@
+package Shmidt.lesson_19;
+
+public class Cat extends Animal {
+    static int counter;
+    static int foodCounter;
+
+    Cat(String name, HealthState health) {
+        this.name = name;
+        this.health = health;
+        food = "Пурина ван";
+        location = "Там, где тепло, есть еда, пакеты и человеки";
+        counter++;
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Meow motherfucka!");
+    }
+
+    @Override
+    void eat() {
+        if (foodCounter == 0)
+            System.out.println("Еды для кошек нет!");
+        else {
+            System.out.println(String.format("Котик ест: %s", food));
+            System.out.println(String.format("Осталось корма: %s", --foodCounter));
+        }
+    }
+
+    void eat(int foodCount) {
+        if (foodCounter == 0)
+            System.out.println("Еды для кошек нет!");
+        else {
+            System.out.println(String.format("Пёсик ест: %s", food));
+            System.out.println(String.format("Осталось костей: %s", foodCounter -= foodCount));
+        }
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("Животное - кот\n" +
+                "\tКличка:" + name + "\n" +
+                "\tСтрана обитания:" + location + "\n" +
+                "\tСостояние здоровья:" + health + "\n" +
+                "\tСколько особей:" + counter + "\n"
+        );
+        /*
+        Формат вывода:
+        <Кличка>
+        Страна обитания: ...
+        Состояние здоровья: ...
+        Сколько особей: ...
+        */
+        return null;
+    }
+}
+/*
+Написать классы Dog, Cat, Horse, которые наследуют Animal
+и переопределяют методы makeSound, eat, toString.
+Создайте класс Vet. Класс содержит параметр:
+- name - имя ветеринара
+Класс содержит методы:
+- void treatAnimal(Animal animal) - осуществляет прием у ветеринара,
+вылечивает здоровье животного
+ */
---
+++ src/Shmidt/lesson_19/Main.java
@@ -0,0 +1,58 @@
+package Shmidt.lesson_19;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cat.foodCounter = 10;
+        Dog.foodCounter = 15;
+        Horse.foodCounter = 123;
+        Cat cat1 = new Cat("Барсик", Animal.HealthState.HEALTHY);
+        cat1.eat();
+        cat1.makeSound();
+        cat1.sleep();
+        cat1.toString();
+
+        Cat cat3 = new Cat("Пикси", Animal.HealthState.HEALTHY);
+        Cat cat2 = new Cat("ЧЁРНАЯТЬМА", Animal.HealthState.HEALTHY);
+        cat2.toString();
+
+
+        Dog dog1 = new Dog("Бобик", Animal.HealthState.UNHEALTHY);
+        Dog dog2 = new Dog("Брут", Animal.HealthState.HEALTHY);
+        dog1.eat(7);
+        dog2.makeSound();
+        dog2.sleep();
+        dog2.toString();
+
+
+        Horse horse1 = new Horse("Сабатон", Animal.HealthState.UNHEALTHY);
+        horse1.eat();
+        horse1.makeSound();
+        horse1.sleep();
+        horse1.toString();
+
+
+        Vet vet = new Vet("Айболит");
+        vet.treatAnimal(horse1);
+        vet.treatAnimal(dog1);
+        vet.treatAnimal(dog2);
+        vet.treatAnimal(cat1);
+        vet.treatAnimal(cat3);
+        horse1.toString();
+        dog1.toString();
+        dog2.toString();
+        cat1.toString();
+        cat3.toString();
+
+    }
+}
+/*
+Создайте класс Main со статическим метод main, в котором:
+
+- добавьте переменные в классов Dog, Cat, Horse
+- выведите внутреннию информацию о каждой особи
+- выведите результат makeSound для каждой особи
+- вызовите метод eat у некоторых животных
+- создайте ветеринара и отведите каждого животного на прием
+- вызовите метод sleep у некоторых животных
+ */
