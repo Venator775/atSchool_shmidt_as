d53d19a8b5f2893de198d8e512b960531a67723f
Aleksey Shmidt <lp8839@yandex.ru>
2023-01-17 22:35:38 +0300

    добавил урок с BigDecimal и класс для тестов

---
+++ src/Shmidt/SomeTests.java
@@ -0,0 +1,18 @@
+package Shmidt;
+
+public class SomeTests {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        int z = 8;
+        x = y = z;
+        z = 10;
+        x = 15;
+        System.out.println(false ^ false);
+        System.out.println(true ^ false);
+        System.out.println(false ^ true);
+        System.out.println(true ^ true);
+
+
+    }
+}
