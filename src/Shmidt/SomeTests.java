afdfe565b966d0b113adef7fd24378b1ec289d89
Aleksey Shmidt <lp8839@yandex.ru>
2023-02-20 13:34:07 +0300

    lesson58 task1-5

--- src/Shmidt/SomeTests.java
+++ src/Shmidt/SomeTests.java
@@ -15,9 +15,67 @@ public class SomeTests {
         if ("Hello" == s)
             System.out.println("4");
 
-        if ("Hello".intern() == t)//почему false?
+
+        if ("Hello".intern() == t)//todo почему false?
             System.out.println("5");*/
 
+
+
+
+
+/*
+        int[] array = new int[]{7, 9, 3, 5};
+        int size = 4;
+        int index, indexout = 0;
+        while (indexout < size) {
+            index = 0;
+            while (index < size - 1) {
+                if (array[index + 1] < array[index]) {
+                    int temp = array[index];
+                    array[index] = array[index + 1];
+                    array[index + 1] = temp;
+                }
+                index++;
+            }
+            indexout++;
+        }
+
+        System.out.println("array:");
+        for (int i : array) {
+            System.out.println(i);
+        }
+
+        int[] array1 = new int[size];
+        for (int i=0; i<size;i++) {
+            array1[i]=array[i];
+        }
+
+        indexout = 0;
+        while (indexout < size) {
+            index = 0;
+            while (index < size - 1) {
+                if (array1[index + 1] > array1[index]) {
+                    int temp = array1[index];
+                    array1[index] = array1[index + 1];
+                    array1[index + 1] = temp;
+                }
+                index++;
+            }
+            indexout++;
+        }
+        System.out.println("array1:");
+        for (int i : array1) {
+            System.out.println(i);
+        }
+
+
+        System.out.println("совместный вывод");
+        for (int i : array) {
+            System.out.println(i);
+        }
+        for (int i : array1) {
+            System.out.println(i);
+        }*/
 
     }
 }
