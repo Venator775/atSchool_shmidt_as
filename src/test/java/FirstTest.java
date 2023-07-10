a8269eb93d06708ce095cd68b418c265870e642c
Aleksey Shmidt <lp8839@yandex.ru>
2023-07-11 00:12:31 +0300

    запущен гредл

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void someTest() {
+        System.out.println("успех");
+        Assertions.assertEquals(4, 2 + 2);
+    }
+
+
+}
