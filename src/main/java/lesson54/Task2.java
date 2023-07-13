5f45afd016476adfd404b9c77f6f3dcecef6abae
Aleksey Shmidt <lp8839@yandex.ru>
2023-07-13 22:43:55 +0300

    запущен первый тест гредл

---
+++ src/main/java/lesson54/Task2.java
@@ -0,0 +1,69 @@
+package lesson54;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task2 {
+    public static void main(String[] args) {
+        if (args[0].equals("\\") || args[0].equals("/")) {
+            System.out.println("Введен некорректный путь до файла.");
+            return;
+        }
+
+        //это тут нужно для обработки пробела в пути
+        String str = "";
+        if (args.length > 1) {
+            StringBuilder finalStr = new StringBuilder(str);
+//            List.of(args).forEach((arg) -> {
+//                finalStr.append(arg + " ");
+//            });
+//            str=finalStr.toString();
+
+            Arrays.stream(args)
+                    .collect(Collectors.toList())
+                    .forEach((arg) -> {
+                finalStr.append(arg + " ");
+            });
+            str=finalStr.toString();
+        }
+        else str = args[0];
+
+        String path = "empty", file = "empty";
+        if (str.contains("\\")) {
+            path = str.substring(0, str.lastIndexOf("\\")+1);
+            file = str.substring(str.lastIndexOf("\\") + 1);
+        } else if (str.contains("/")) {
+            path = str.substring(0, str.lastIndexOf("/")+1);
+            file = str.substring(str.lastIndexOf("/") + 1);
+        }
+
+        if(file.equals("")||file.equals(" "))
+            file = "Введен некорректный путь до файла.";
+
+        System.out.println("path=" + path);
+        System.out.println("file=" + file);
+    }
+}
+/*
+Через аругмент командной строки передается строка - путь до некоторого файла -
+содержащая знак «/» или «\». Например, "/bin/java" или "C:\Program Files\Java\bin\java".
+
+Напишите программу, которая для обоих вариантов разделителей, может вывести отдельно путь и имя файла.
+Если имя файла отсутствует, т.е. строка оканчивается на "\" или "/",
+то выдать ошибку: "Введен некорректный путь до файла."
+
+Пример:
+1) Через аргументы передается "/bin/java", выводится:
+Путь: /bin/
+Имя файла: java
+
+2) Через аргументы передается "C:\Program Files\Java\bin\java", выводится:
+Путь: C:\Program Files\Java\bin\
+Имя файла: java
+
+3) Через аргументы передается "/", выводится:
+Введен некорректный путь до файла.
+Проверку на наличие и корректность аргумента делать не надо.
+ */
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static lesson54.Task1.method;
+
 public class FirstTest {
 
     @Test
-    void someTest() {
-        System.out.println("успех");
-        Assertions.assertEquals(4, 2 + 2);
+    void les54Test() {
+        System.out.println("Запуск les54Test");
+        String[] shit = new String[]{"а"};
+        Assertions.assertEquals(7, method(shit));
     }
 
 
