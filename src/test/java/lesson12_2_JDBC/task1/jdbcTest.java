17d2959b54444cc7ed417cd584d7ee46f92d6383
Aleksey Shmidt <lp8839@yandex.ru>
2023-07-26 01:43:31 +0300

    добавлен успешный запуск всех трёх тестов

--- src/test/java/lesson12_2_JDBC/task1/jdbcTest.java
+++ src/test/java/lesson12_2_JDBC/task1/jdbcTest.java
@@ -5,18 +5,22 @@ import lesson12_2_JDBC.task1.Director.Director;
 import lesson12_2_JDBC.task1.Director.DirectorRepositoryImpl;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.stream.Stream;
+import java.time.ZoneId;
+import java.util.ArrayList;
 
 public class jdbcTest {
     final private static String dbName = "testprojectjdbc";
     final private static String dbUser = "postgres";
     final private static String dbPassword = "123456";
+
+    private static ArrayList<Director> tempDirectors;
 
     private static Connection connection;
 
@@ -25,29 +29,66 @@ public class jdbcTest {
         Connection connectionTemp = ConnectToDB.InitDBConnection(dbName, dbUser, dbPassword);
         Assertions.assertNotNull(connectionTemp, "Не удалось подключиться к БД");
         connection = connectionTemp;
+        tempDirectors = new ArrayList<>();
     }
 
 
     @ParameterizedTest(name = "{displayName} - {0} ({2})")
     @DisplayName("Тест select")
-    @MethodSource("lesson12_2_JDBC.task1.testDataProviders.TestDataProvider#directorsProvider")
-    @Tag("select")
+    @MethodSource("lesson12_2_JDBC.task1.testDataProviders.TestDataProvider#directorsFromDBProvider")
+    @Tag("lesson12_2_JDBCtask1")
     void selectTest(int id, String firstName, String lastName, LocalDate birthDate, String country) {
         Director expectedDirector = new Director(id, firstName, lastName, birthDate, country);
 
         DirectorRepositoryImpl dri = new DirectorRepositoryImpl(connection);
         Director selectedDirector = dri.get(id);
         Assertions.assertNotNull(selectedDirector, "Не удалось выбрать запись");
-        Assertions.assertAll(
-                () -> Assertions.assertEquals(expectedDirector.getId(), selectedDirector.getId()),
-                () -> Assertions.assertEquals(expectedDirector.getFirstName(), selectedDirector.getFirstName()),
-                () -> Assertions.assertEquals(expectedDirector.getLastName(), selectedDirector.getLastName()),
-                () -> Assertions.assertEquals(expectedDirector.getBirthDate(), selectedDirector.getBirthDate()),
-                () -> Assertions.assertEquals(expectedDirector.getCountry(), selectedDirector.getCountry())
-        );
+        assertDirectorsAreEqual(expectedDirector, selectedDirector);
 
     }
 
+    @ParameterizedTest(name = "{displayName} - {0} ({2})")
+    @MethodSource("lesson12_2_JDBC.task1.testDataProviders.TestDataProvider#newDirectorsProvider")
+    @DisplayName("Тест insert")
+    @Tag("lesson12_2_JDBCtask1")
+    void insertTest(int id, String firstName, String lastName, LocalDate birthDate, String country) {
+        Director expectedDirector = new Director(id, firstName, lastName, birthDate, country);
+
+        DirectorRepositoryImpl dri = new DirectorRepositoryImpl(connection);
+        dri.save(expectedDirector);
+        Director addedDirector = dri.get(id);
+        assertDirectorsAreEqual(expectedDirector, addedDirector);
+
+        tempDirectors.add(expectedDirector);
+    }
+
+    @Test
+    @DisplayName("Тест delete")
+    @Tag("lesson12_2_JDBCtask1")
+    void deleteTest() {
+        DirectorRepositoryImpl dri = new DirectorRepositoryImpl(connection);
+        String query = "select * from directors where id > ? limit ?";
+        PreparedStatement psSelect;
+        ArrayList<Director> tempDirectors = new ArrayList<>();
+        try {
+            psSelect = connection.prepareStatement(query);
+            psSelect.setInt(1, 10);
+            psSelect.setInt(2, 3);
+            psSelect.execute();
+            ResultSet executionResult = psSelect.getResultSet();
+            while (executionResult.next()){
+                tempDirectors.add(new Director(executionResult.getInt(1),
+                        executionResult.getString(2),
+                        executionResult.getString(3),
+                        LocalDate.parse(executionResult.getString(4)),
+                        executionResult.getString(5)));
+            }
+            tempDirectors.forEach(dri::delete);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @AfterAll
     static void closeConnection() {
@@ -55,6 +96,27 @@ public class jdbcTest {
             connection.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private static void assertDirectorsAreEqual(Director expectedDirector, Director factDirector) {
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(expectedDirector.getId(), factDirector.getId()),
+                () -> Assertions.assertEquals(expectedDirector.getFirstName(), factDirector.getFirstName()),
+                () -> Assertions.assertEquals(expectedDirector.getLastName(), factDirector.getLastName()),
+                () -> Assertions.assertEquals(expectedDirector.getBirthDate(), factDirector.getBirthDate()),
+                () -> Assertions.assertEquals(expectedDirector.getCountry(), factDirector.getCountry())
+        );
+    }
+
+    private void showResultSet(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String fName = resultSet.getString("first_name");
+            String lName = resultSet.getString("last_name");
+            LocalDate birthDate = resultSet.getDate("birth_date").toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            String country = resultSet.getString("country");
+            System.out.printf("%d %s %s %s %s\n", id, fName, lName, birthDate, country);
         }
     }
 }
--- src/test/java/lesson12_2_JDBC/task1/testDataProviders/TestDataProvider.java
+++ src/test/java/lesson12_2_JDBC/task1/testDataProviders/TestDataProvider.java
@@ -1,18 +1,30 @@
 package lesson12_2_JDBC.task1.testDataProviders;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.params.provider.Arguments;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.stream.Stream;
 
+import static Shmidt.tests.sideMethods.rnd;
+
 public class TestDataProvider {
-    private static Stream<Arguments> directorsProvider() {
+    private static Stream<Arguments> directorsFromDBProvider() {
         return Stream.of(
                 Arguments.arguments(1, "Anakin", "Skywalker", LocalDate.of(2000, 1, 1), "Tatooine"),
                 Arguments.arguments(2, "Obi-wan", "Kenobi", LocalDate.of(2023, 7, 24), "Stewjon"),
                 Arguments.arguments(4, "Jar Jar", "Binks", LocalDate.of(1967, 6, 22), "Naboo"),
                 Arguments.arguments(5, "Jar Jar2", "Binks", LocalDate.of(1967, 6, 22), "Naboo2"),
                 Arguments.arguments(6, "Jar Jar1", "Binks", LocalDate.of(1967, 6, 22), "Naboo1")
+        );
+    }
+
+    private static Stream<Arguments> newDirectorsProvider() {
+        return Stream.of(
+                Arguments.arguments(rnd(10,14), new Faker().name().firstName(), new Faker().funnyName().name(), new Faker().date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), new Faker().country().name()),
+                Arguments.arguments(rnd(15,17), new Faker().name().firstName(), new Faker().funnyName().name(), new Faker().date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), new Faker().country().name()),
+                Arguments.arguments(rnd(18,20), new Faker().name().firstName(), new Faker().funnyName().name(), new Faker().date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), new Faker().country().name())
         );
     }
 }
